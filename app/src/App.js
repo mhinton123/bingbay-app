383661099784f1597262b6f6b8e7cca9c44cd6cf
mhinton123 <matthewhinton@hotmail.co.uk>
2024-04-08 20:46:02 +0100

    complete 1.0

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, NavLink} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, NavLink, useLocation } from "react-router-dom";
 import './App.css';
 
 import HomePage from "./pages/HomePage"
 import MoviesPage from "./pages/MoviesPage"
 import SeriesPage from "./pages/SeriesPage"
 import LoginPage from "./pages/LoginPage"
+import SignUpPage from "./pages/SignUpPage"
 
 import Logo from "./assets/logo.svg";
 import NavHomeIcon from "./assets/icon-nav-home.svg";
@@ -18,59 +19,69 @@ import data from "./data.json"
 const DataContext = React.createContext()
 
 function App() {
-
   // tracks whether the user is searching or not
   const [searchValue, setSearchValue] = React.useState(false)
   // tracks the current page the browser is on
   const [page, setPage] = React.useState(null)
 
-  React.useEffect(()=> setPage("Home"), [])
 
   // reset search value every time different page loads
-  React.useEffect(()=> setSearchValue(""), [page])
+  React.useEffect(()=> {
+    setSearchValue("");
+  }, [page])
+
+  // Conditionally render the navbar based on the current page
+  const renderNavbar = () => {
+    if (page === "login") {
+      return null; // Don't render the navbar on the login page
+    }
+    return (
+      <header>
+        <nav>
+          <div className="navbar-wr">
+            <div className="nav-icons-wr">
+              <NavLink to="/">
+                <img className="logo" src={Logo} alt="logo" />
+              </NavLink>
+              <NavLink 
+                className={({isActive}) => isActive ? "active-link" : "" }
+                to="/">
+                <img className="nav-icon" src={NavHomeIcon} alt="Home Icon" />
+              </NavLink>
+              <NavLink 
+                className={({isActive}) => isActive ? "active-link" : "" }
+                to="/movies">
+                <img className="nav-icon" src={NavMoviesIcon} alt="Movies Icon" />
+              </NavLink>
+              <NavLink 
+                className={({isActive}) => isActive ? "active-link" : "" }
+                to="/series">
+                <img className="nav-icon" src={NavSeriesIcon} alt="Series Icon" />
+              </NavLink>
+            </div>
+            <NavLink to="/login">
+              <img className="avatar-icon" src={AvatarIcon} alt="Avatar Icon" />
+            </NavLink>
+          </div>
+        </nav>
+      </header>
+    );
+  };
 
   return (
     <div className="App">
-      <Router>
-        <header>
-          <nav>
-            <div className="navbar-wr">
-              <img className="logo" src={Logo} alt="logo" />
-              <div className="nav-icons-wr">
-                <NavLink 
-                  className={({isActive}) => isActive ? "active-link" : "" }
-                  to="/">
-                  <img className="nav-icon" src={NavHomeIcon} alt="Home Icon" />
-                </NavLink>
-                <NavLink 
-                  className={({isActive}) => isActive ? "active-link" : "" }
-                  to="/movies">
-                  <img className="nav-icon" src={NavMoviesIcon} alt="Movies Icon" />
-                </NavLink>
-                <NavLink 
-                  className={({isActive}) => isActive ? "active-link" : "" }
-                  to="/series">
-                  <img className="nav-icon" src={NavSeriesIcon} alt="Series Icon" />
-                </NavLink>
-              </div>
-              <img className="avatar-icon" src={AvatarIcon} alt="Avatar Icon" />
-            </div>
-          </nav>
-        </header>
+      <Router> {/* Wrap your App component with Router */}
+        {renderNavbar()} {/* Render the navbar */}
         <main>
-        <DataContext.Provider value={
-                  {data, 
-                  searchValue, 
-                  setSearchValue, 
-                  page, 
-                  setPage}
-        }>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/movies" element={<MoviesPage />} />
-            <Route path="/series" element={<SeriesPage />} />
-          </Routes>
-        </DataContext.Provider>  
+          <DataContext.Provider value={{ data, searchValue, setSearchValue, page, setPage }}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/movies" element={<MoviesPage />} />
+              <Route path="/series" element={<SeriesPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
+            </Routes>
+          </DataContext.Provider>  
         </main>
       </Router>
     </div>
--- app/src/pages/SignUpPage.jsx
+++ app/src/pages/SignUpPage.jsx
@@ -5,15 +5,16 @@ export default function LoginPage(){
     return (
         <div className="login-page-wr">
             <div className="form-wr">
-                <h2 className="heading-l">Login</h2>
+                <h2 className="heading-l">Sign Up</h2>
                 <form action="###">
                     <input type="email" className="login-input" placeholder="Email Address" />
                     <input type="password" className="login-input" placeholder="Password" />
-                    <button type="submit" className="submit-btn">Login to your account</button>
+                    <input type="password" className="login-input" placeholder="Confirm Password" />
+                    <button type="submit" className="submit-btn">Create an account</button>
                     <div className="signup-link-wr">
-                        <p className="body-s">Don't have an account?</p>
-                        <NavLink to="/signup">
-                            <a href="" className="body-s">Sign Up</a>
+                        <p className="body-s">Already have an account?</p>
+                        <NavLink to="/login">
+                            <a href="" className="body-s">Login</a>
                         </NavLink>
                     </div>
                 </form>
